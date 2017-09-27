1feadfc79e2897f2325c92d133a38bc28a3ce042
DipanshuShora <deepanshushora478@gmail.com>
2017-09-27 20:43:26 +0530

    install proptypes package

--- src/app/InputField.js
+++ src/app/InputField.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {IntroList} from './IntroList.js';
+
 export class InputField extends React.Component{
     constructor(props){
         super(props);
         this.state = {user_name:'Enter Your Name',user_ph:'Enter Your Phone Number',user_adult:0};
     }
     render(){
+        var list = {
+            name:this.state.user_name,
+            ph:this.state.user_ph,
+            adult:this.state.user_adult
+        };
         return(
             <div>
                 <input type="text" onChange={(e)=>this.setState({user_name:e.target.value})} placeholder={this.state.user_name} /><br/>
                 <input type="text" onChange={(e)=>this.setState({user_ph:e.target.value})} placeholder={this.state.user_ph} /><br/>
                 <input type="text" onChange={(e)=>this.setState({user_adult:e.target.value})} placeholder={this.state.user_adult} />
-                <IntroList name={this.state.user_name} ph={this.state.user_ph} adult={this.state.user_adult}/>
+                <IntroList list={list}/>
             </div>
         );
     }
--- src/app/IntroList.js
+++ src/app/IntroList.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 export const IntroList = (props) =>{
     return(
         <p>
-            Name : {props.name}<br />
-            Phone Number : {props.ph}<br />
-            Voting Eligibility : {props.adult>=18?'Eligible':'Not Eligible'}
+            Name : {props.list.name}<br />
+            Phone Number : {props.list.ph}<br />
+            Voting Eligibility : {props.list.adult>=18?'Eligible':'Not Eligible'}
         </p>
     );
-}
+};
+IntroList.propTypes = {
+    name:PropTypes.string,
+    ph:PropTypes.number,
+    adult:PropTypes.number
+};
