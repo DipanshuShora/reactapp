7e6f729c2eba8923feb428a9247707f00c1a147d
DipanshuShora <deepanshushora478@gmail.com>
2017-11-03 12:34:52 +0530

    removed all

--- src/app/Heading.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const Heading  = function (){
-    return (
-        <div>Hello World</div>
-    );
-};
--- src/app/MyButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function handleClick(){
-    alert("Example of Global Function");
-}
-export const MyButton = () => {
-    return(
-        <button onClick={handleClick}>Click Me Plz</button>
-    );
-}
